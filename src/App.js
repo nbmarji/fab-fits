affd089fda7717d098854cd2548fcb95660dd3ea
nbmarji <nbmarji@gmail.com>
2021-01-20 20:03:24 +0100

    using HashRouter

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import LooksMainPage from "./pages/looksMainPage.js"
 import LookPage from "./pages/lookpage.js"
 import React from "react";
 import "./App.css";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {HashRouter, BrowserRouter as Router, Route} from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
+      <HashRouter>
       <Router>
        <Route exact path="/" component={Homepage} />
        <Route exact path = "/contact" component={Contact}/>
        <Route exact path ="/looks" component= {LooksMainPage}/>
        <Route exact path="/look/:lookId" component={LookPage} />
       </Router>
+      </HashRouter>
     );
   }
 }
